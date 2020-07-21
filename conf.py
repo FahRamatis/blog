72538c7f3b1cd3a4d119d7e9a50630b7a66ddd79
FahRamatis <fah.ramatis@gmail.com>
2020-07-21 17:56:11 -0300

    correct url

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Fabio Ramatis"  # (translatable)
 BLOG_TITLE = "Fah Blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://framatis.com/cherry-blog/"
+SITE_URL = "http://framatis.com/blog/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
